2ac073ca4e8c1aa74bb9395d920cbb5494a61981
Han Nim Jang <josephnimz@gmail.com>
2015-02-18 22:46:29 +0900

    edited payment...

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,17 +37,17 @@ class OrdersController < ApplicationController
         :currency => "usd",
         :card => token
         )
-      flash[:notice] = "구매해주셔서 감사합니다."
+      flash[:notice] = "Thanks for ordering!"
     rescue Stripe::CardError => e
       flash[:danger] = e.message
     end
-
+    
     respond_to do |format|
       if @order.save
         format.html { redirect_to root_url }
-        format.json { render :show, status: :created, location: @order }
+        format.json { render action: 'show', status: :created, location: @order }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
